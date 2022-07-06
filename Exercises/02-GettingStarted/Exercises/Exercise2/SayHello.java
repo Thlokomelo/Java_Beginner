e33ca01131ded7a8f3cd2cdf039638e24a896598
Thlokomelo Hlomuka <98019737+Thlokomelo@users.noreply.github.com>
2022-07-06 23:24:58 +0200

    Added Exercises Folder with ex 2 - 5

---
+++ Exercises/02-GettingStarted/Exercises/Exercise2/SayHello.java
@@ -0,0 +1,12 @@
+
+public class SayHello {
+    public static void main(String[] args) {
+        
+        System.out.println("Total is: " +(args[0] +args[1]));
+        int arg1 = Integer.parseInt(args[0]);
+        int arg1 = Integer.parseInt(args[1]);
+        System.out.println("Total is: " + (arg1+arg2));
+    }
+}
+    
+

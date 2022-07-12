a6dd25c2e2186493d18bf00711535bbea724fb26
Thlokomelo Hlomuka <98019737+Thlokomelo@users.noreply.github.com>
2022-07-12 13:52:51 +0200

    Added Book 2 project with Listing 1-1

---
+++ DummiesBook/Book2/src/Listing_1_1/HelloApp.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Listing_1_1;
+
+public class HelloApp {	                       	
+    public static void main(String[] args)
+    {	
+        System.out.println("Hello, World!");
+    }	
+}
+
+
+/* public: 
+makes the class element accessible to other Java elements */
+
+/*Class:
+contains code that defines the behavior of the
+objects created and used by the program. */
+
+/* HelloApp: 
+An identifier that provides the name for the class being defined
+here */
+
+/*public static void main(String[] args):
+- public: allows piblic access
+- Static: Required when declaring main method (to be discuscced in chp03)
+- Void: Specifies that a value does not need to return a value
+- Main: the name of the method
+- (String[] args): parameter list, and it’s used to pass data
+to a method. The parameter is an Array of String objects. The parameter is
+named args
+*/
+
+/* System.out.println("Hello,World!"):
+- Statement that calls a method called println that belongs to the
+System.out object. Prints the string literal "Hello, World" to the console
+*/
+
+

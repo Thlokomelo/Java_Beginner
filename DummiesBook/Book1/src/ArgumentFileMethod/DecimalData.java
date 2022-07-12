6cf7113f9d94863e3e974a2a295439f3daf48537
Thlokomelo Hlomuka <98019737+Thlokomelo@users.noreply.github.com>
2022-07-12 02:05:38 +0200

    Updated Book 1 exercises

---
+++ DummiesBook/Book1/src/ArgumentFileMethod/DecimalData.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author thlok
+ */
+public class DecimalData {
+
+    public static void main(String[] args) {
+        double x = 5.02;
+        double y = 0.01;
+        double z = x + y;
+        System.out.println(z);
+    }
+
+}
+
+//Java weakness: does not directly support true decimal data
+//Requires special coding
+//Answer should be 5.03
---
+++ DummiesBook/Book1/src/CompilingMoreThanOneFile/HelloApp.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+public class HelloApp {                        //class name
+    public static void main(String[] args) {   //method: main (JVM Calls this when running prgm)
+        System.out.println("Hello, World");  
+    }   
+}
+
+//require each executable statement to end with a semicolon (;)
+//use braces ({}) to mark blocks of code.
+//use a routine called main as the main entry point for the program
+
+//sout:
+//uses the out field of the System class to get a PrintStream object.
+//calls the println method of that object to write a line to the console.

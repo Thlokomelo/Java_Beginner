5041b3bb1205551c48406ad4975704e0badbb3e6
Thlokomelo Hlomuka <98019737+Thlokomelo@users.noreply.github.com>
2022-08-05 13:58:42 +0200

    Updated Book 3 with Chapter 1 Notes

---
+++ DummiesBook/Book3/src/Chapter_1_Object_Oriented_Programming/Understanding_Objects.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Chapter_1_Object_Oriented_Programming;
+
+
+/*
+Page: 237
+
+What are Objects?
+
+- both in the real world and in the world of programming — are entities that have
+  certain basic characteristics.
+
+Charaacteristics of Objects:
+
+- Objects have Identity
+
+For example:
+every occurrence of a particular type of object — called an instance — can be distinguished
+from every other occurrence of the same type of object, as well as from objects of other types.
+
+Another Example:
+If you have multiple invoices, each invoice can be identified by a unique invoice number.
+
+NB: 
+ » Each object has its own location in the computer’s memory.
+ » The address of the starting location for an object provides a way of distinguishing one object 
+   from another, because no two objects can occupy the same location in memory.
+ » Java objects have something called a hash code, the hash code for an object is based on
+   the object’s memory address.
+
+- Objects have Type
+
+ » object-oriented programming lets you assign names to the different kinds of objects in a program.
+ » In Java, types are defined by classes.
+
+Example: Statement creates an object of type Invoice
+
+            Invoice i = new Invoice();
+
+
+
+
+
+
+*/

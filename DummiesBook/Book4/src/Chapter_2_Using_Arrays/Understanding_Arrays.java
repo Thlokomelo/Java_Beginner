1585674d8e313053bde0249844632852d46d97de
Thlokomelo Hlomuka <98019737+Thlokomelo@users.noreply.github.com>
2022-09-06 13:05:42 +0200

    Updated Book 4 with chapter 2 notes

---
+++ DummiesBook/Book4/src/Chapter_2_Using_Arrays/Understanding_Arrays.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Chapter_2_Using_Arrays;
+
+/*
+Page: 393
+
+Understanding Arrays
+--------------------
+
+» An array is a set of variables that is referenced by using a single variable name
+  combined with an index number. Each item of an array is called an element. 
+» All the elements in an array must be of the same type. Thus the array itself has a type
+  that specifies what kind of elements it can contain. An int array can contain int values, for example, 
+  and a String array can contain strings.
+
+
+Some information on arrays:
+
+» An array is itself an object. You can refer to the array object as a whole, rather
+  than to a specific element of the array, by using the array’s variable name without
+  an index. Thus, if x[5]refers to an element of an array, x refers to the array itself.
+
+» An array has a fixed length that’s set when the array is created. This length
+  determines the number of elements that can be stored in the array. The maximum
+  index value you can use with any array is one less than the array’s length.
+  Thus, if you create an array of ten elements, you can use index values from 0 to 9.
+
+» You can’t change the length of an array after you create the array.
+
+» You can access the length of an array by using the length field of the array
+  variable. x.length, for example, returns the length of the array x.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ */

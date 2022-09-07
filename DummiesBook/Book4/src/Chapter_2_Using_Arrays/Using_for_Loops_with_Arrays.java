01e2e815ed48f9d3f936d6825ac2db41a84d23fd
Thlokomelo Hlomuka <98019737+Thlokomelo@users.noreply.github.com>
2022-09-07 11:40:45 +0200

    Updated Book 4 with chapter 2

---
+++ DummiesBook/Book4/src/Chapter_2_Using_Arrays/Using_for_Loops_with_Arrays.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Chapter_2_Using_Arrays;
+
+/*
+Page:  396
+
+Using for Loops with Arrays
+---------------------------
+
+» One of the most common ways to process an array is with a for loop. In fact, for
+  loops were invented specifically to deal with arrays. See Numbers class.
+
+ */
+

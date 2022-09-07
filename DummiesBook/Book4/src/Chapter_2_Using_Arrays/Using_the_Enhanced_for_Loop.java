a2f669e3958a9beef911b664b0881529468ec938
Thlokomelo Hlomuka <98019737+Thlokomelo@users.noreply.github.com>
2022-09-07 11:42:12 +0200

    Updated Book 4 with chapter 2 notes

---
+++ DummiesBook/Book4/src/Chapter_2_Using_Arrays/Using_the_Enhanced_for_Loop.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Chapter_2_Using_Arrays;
+
+/*
+Page: 400
+
+Using the Enhanced for Loop
+---------------------------
+
+The enhanced for loop is designed to simplify loops that process arrays and collections.
+
+Format:
+------
+
+for (type identifier : array)   //type
+{
+Statements ...
+}
+
+» The type identifies the type of the elements in the array.
+» The identifier provides a name for a local variable that is used to access each element. 
+» Array names the array you want to process.
+
+
+Example:
+-------
+
+see DaysOfTheWeek class as an example.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ */
+

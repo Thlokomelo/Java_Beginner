4f75b5107c9b561347b0769f1b57c18da3ced4c8
Thlokomelo Hlomuka <98019737+Thlokomelo@users.noreply.github.com>
2022-09-08 13:53:30 +0200

    Updated Book 4 with chapter 3 notes

---
+++ DummiesBook/Book4/src/Chapter_3_Using_the_ArrayList_Class/Signs.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Chapter_3_Using_the_ArrayList_Class;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author thlok
+ */
+public class Signs {
+
+    public static void main(String[] args) {
+        // ArrayList signs = new ArrayList();
+        ArrayList<String> signs = new ArrayList<String>();
+
+        signs.add("Drink Pepsi");
+        signs.add("No minors allowed");
+        signs.add("Say Pepsi, Please");
+        signs.add("7-Up: You Like It, It Likes You");
+        signs.add("Dr. Pepper 10, 2, 4");
+        
+        System.out.println(signs);
+
+    }
+
+}

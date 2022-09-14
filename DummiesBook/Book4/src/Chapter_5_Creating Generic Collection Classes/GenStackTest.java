20f927c82d6c37f3befe1f2f97c482169d9300cc
Thlokomelo Hlomuka <98019737+Thlokomelo@users.noreply.github.com>
2022-09-14 11:46:03 +0200

    Updated Book 4 with chapter 5 notes

--- DummiesBook/Book4/src/Chapter_5_Creating Generic Collection Classes/GenStackTest.java
+++ DummiesBook/Book4/src/Chapter_5_Creating Generic Collection Classes/GenStackTest.java
@@ -11,26 +11,31 @@ package Chapter_5_Creating_Generic_Collection_Classes;
 public class GenStackTest {
 
     public static void main(String[] args) {
-        GenStack<String> gs = new GenStack<String>();
+        GenStack<String> gs = new GenStack<String>();              //GenStack object created to hold string objects
         System.out.println(
                 "Pushing four items onto the stack.");
-        gs.push("One");
+        gs.push("One");                                            //Strings are pushed onto the stack
         gs.push("Two");
         gs.push("Three");
         gs.push("Four");
-        System.out.println("There are "
+        System.out.println("There are "                          //Prints the number of items in the stack
                 + gs.size() + " items in the stack.\n");
-        System.out.println("The top item is: "
+        
+        System.out.println("The top item is: "                  //prints the top item
                 + gs.peek() + "\n");
-        System.out.println("There are still "
+        
+        System.out.println("There are still "                    //Prints the number of items in the stack
                 + gs.size() + " items in the stack.\n");
+        
         System.out.println("Popping everything:");
+        
         while (gs.hasItems()) {
-            System.out.println(gs.pop());
+            System.out.println(gs.pop());                     //loop uses the pop method to pop each item off the stack and print it.
         }
-        System.out.println("There are now "
+        System.out.println("There are now "                   //Prints the number of items in the stack
                 + gs.size() + " items in the stack.\n");
-        System.out.println("The top item is: "
+        
+        System.out.println("The top item is: "               //prints the top item
                 + gs.peek() + "\n");
     }
 }

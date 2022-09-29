8c3d6e56f577456a8a7584b199c5e8c82e544a5f
Thlokomelo Hlomuka <98019737+Thlokomelo@users.noreply.github.com>
2022-09-29 08:37:16 +0200

    Updated Book 5 with Chapter 3 notes

---
+++ DummiesBook/Book5/src/Chapter_3_Using_Recursion/Notes.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Chapter_3_Using_Recursion;
+
+/*
+Page: 523
+
+Using Recursion
+---------------
+
+Recursion is a basic programming technique in which a method calls itself to
+solve some problem.
+
+Calculating the Classic Factorial Example
+-----------------------------------------
+
+Example: The factorial of any given integer i.e. n, is the product of all the integers from 1 to n.
+         Therefore the factorial of 5 is 120, that is 5 × 4 × 3 × 2 × 1.
+
+The nonrecursive solution
+-------------------------
+
+see NonRecursive class as an example.
+
+The recursive solution
+----------------------
+
+The recursive solution is based on the notion that the factorial for any number n
+is equal to n times the factorial of n – 1, provided that n is greater than 1. If n is 1,
+the factorial of n is 1.
+
+see Recursive class as an example.
+
+
+Displaying Directories
+----------------------
+
+See DirList Class as an example.
+
+
+
+Using the sort, partition and swap methods
+------------------------------------------
+
+See Sort class as an example
+
+And 
+
+QuickSortApp class as an example
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ */
+

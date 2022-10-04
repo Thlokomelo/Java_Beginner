9d20d117ea95a37bffd186193ae6f998904225e4
Thlokomelo Hlomuka <98019737+Thlokomelo@users.noreply.github.com>
2022-10-04 23:26:35 +0200

    Updated Book 6 with chapter 2 notes

---
+++ DummiesBook/Book6/src/Chapter_2_Handling_Events/AddSubtract5.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Chapter_2_Handling_Events;
+
+/*
+Page: 599
+
+This version of the program caters for a situation where the processing required for one or more
+of the button clicks required hundreds of lines of Java code to implement. Here the actual processing is isolated
+and is done by the by an event handler in a separate method. Then, the lambda expression itself includes just one
+line of code that simply calls the method.
+ */
+
+import javafx.application.*;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.event.*;
+
+public class AddSubtract5 extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    Button btnAdd;
+    Button btnSubtract;
+    Label lbl;
+    int iCounter = 0;
+
+    @Override
+    public void start(Stage primaryStage) {
+        // Create the Add button
+        btnAdd = new Button();
+        btnAdd.setText("Add");
+        btnAdd.setOnAction(e -> btnAdd_Click());
+
+        // Create the Subtract button
+        btnSubtract = new Button();
+        btnSubtract.setText("Subtract");
+        btnSubtract.setOnAction(e -> btnSubtract_Click());
+
+        // Create the Label
+        lbl = new Label();
+        lbl.setText(Integer.toString(iCounter));
+
+        // Add the buttons and label to an HBox pane
+        HBox pane = new HBox(10);
+        pane.getChildren().addAll(lbl, btnAdd, btnSubtract);
+
+        // Add the layout pane to a scene
+        Scene scene = new Scene(pane, 200, 75);
+
+        // Add the scene to the stage, set the title
+        // and show the stage
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Add/Sub");
+        primaryStage.show();
+    }
+
+    private void btnAdd_Click() {
+        iCounter++;
+        lbl.setText(Integer.toString(iCounter));
+    }
+
+    private void btnSubtract_Click() {
+        iCounter--;
+        lbl.setText(Integer.toString(iCounter));
+    }
+
+}

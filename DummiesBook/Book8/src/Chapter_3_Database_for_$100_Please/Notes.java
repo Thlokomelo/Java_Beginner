590e5cffd23b8d6e611994421683f7bad7e3cf43
Thlokomelo Hlomuka <98019737+Thlokomelo@users.noreply.github.com>
2022-10-12 12:28:23 +0200

    Updated Book 8 with chapter 3 notes

---
+++ DummiesBook/Book8/src/Chapter_3_Database_for_$100_Please/Notes.java
@@ -0,0 +1,77 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Chapter_3_Database_for_$100_Please;
+
+/*
+Page: 827
+
+SQL stands for Structured Query Language. It’s the standard language used for creating and accessing 
+relational databases and is the foundation of database processing in Java.
+
+Java provides Java Database Connectivity (JDBC), which lets you formulate SQL
+statements, send them off to a database server, and process the results.
+
+Defining a Relational Database
+------------------------------
+
+A relational database can be:
+
+- A database in which data is stored in tables: Relationships can be established  between tables based on common information. 
+
+- A database that is accessed via SQL: IBM invented SQL to provide a practical way to access data stored in relational databases.
+
+Introducing SQL Statements
+--------------------------
+
+Unlike Java, SQL is not object-oriented. Like Java, however, SQL uses statements to get work done.
+
+*See Table 3-1 on page 829 for SQL statements you use most often.
+
+SQL Statement   Description
+----------------------------------------------------------------------------------------------------
+Data Manipulation
+----------------------------------------------------------------------------------------------------
+select          Retrieves data from one or more tables. This statement is the one you use most often.
+insert          Inserts one or more rows into a table.
+delete          Deletes one or more rows from a table.
+update          Updates existing rows in a table.
+-----------------------------------------------------------------------------------------------------
+Data Definition
+-----------------------------------------------------------------------------------------------------
+create          Creates tables and other database objects.
+alter           Changes the definitions of a table or other database object.
+drop            Deletes a table or other database object.
+use             In scripts, indicates what database subsequent statements apply to.
+
+Creating a SQL Database
+-----------------------
+
+Before you can store data in a relational database, you must create the database.
+Normally you don’t do that from a Java program. Instead, you do it by writing a
+script file that contains the Create statements necessary to create the table and
+then running the script through the database server’s administration program.
+
+*See PGAdmin file as an example.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ */
+

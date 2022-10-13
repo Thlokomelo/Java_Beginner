7546b4756099d1a36980309c825a28279a52a669
Thlokomelo Hlomuka <98019737+Thlokomelo@users.noreply.github.com>
2022-10-13 11:58:26 +0200

    Updated Book 8 with chpater 4 notes

---
+++ DummiesBook/Book8/src/Chapter_4_Using_JDBC_to_Connect_to_a_Database/Notes.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Chapter_4_Using_JDBC_to_Connect_to_a_Database.Notes;
+
+/*
+Page: 845
+
+JDBC — Java Database Connectivity — is a Java feature that lets you connect to
+almost any relational database system, execute SQL commands, and process
+the results, all from within a Java program.
+
+
+Setting Up a Driver
+-------------------
+
+Before you can write a Java program to access a database via JDBC, you must
+first install a driver that links Java’s database API classes to an actual database.
+
+www.mysql.com/products/connector and click the link for JDBC Driver for MySQL (Connector/J).
+
+Connecting to a Database
+------------------------
+
+see postgresql database as an example.
+
+ */

6bb3a97ef9ee850450f8fbf301f54332e38ba3bb
Thlokomelo Hlomuka <98019737+Thlokomelo@users.noreply.github.com>
2022-10-17 12:09:34 +0200

    Updated Book 8 notes

---
+++ DummiesBook/Book8/src/Fx/src/fx/ClickMe.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fx;
+
+/**
+ *
+ * @author thlok
+ */
+import javafx.application.*;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+
+public class ClickMe extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    Button btn;
+
+    @Override
+    public void start(Stage primaryStage) {
+        // Create the button
+        btn = new Button();
+        btn.setText("Click me please!");
+        btn.setOnAction(e -> buttonClick());
+
+        // Add the button to a layout pane
+        BorderPane pane = new BorderPane();
+        pane.setCenter(btn);
+        // Add the layout pane to a scene
+        Scene scene = new Scene(pane, 300, 250);
+
+        // Finalize and show the stage
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("The Click Me App");
+        primaryStage.show();
+    }
+
+    public void buttonClick() {
+        if (btn.getText() == "Click me please!") {
+            btn.setText("You clicked me!");
+        } else {
+            btn.setText("Click me please!");
+        }
+    }
+}
